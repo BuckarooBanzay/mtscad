5e281482b4ec995ea3c528b2da0003c336840653
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-27 19:37:58 +0200

    cylinder

--- context/cylinder.lua
+++ context/cylinder.lua
@@ -1,9 +1,10 @@
-function mtscad.Context:dome(radius, hollow)
+function mtscad.Context:cylinder(radius, height, hollow)
+	radius = math.floor(radius + 0.5)
 	local min_radius, max_radius = radius * (radius - 1), radius * (radius + 1)
 	for z = -radius, radius do
-		for y = 0, radius do
+		for y = 0, height-1 do
 			for x = -radius, radius do
-				local squared = x * x + y * y + z * z
+				local squared = x * x + z * z
 				if squared <= max_radius and (not hollow or squared >= min_radius) then
 					self
 					:translate(x, y, z)
