1056bca3ade265809cc6ee9fd66741d5fcc00593
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-27 19:29:15 +0200

    add cube/sphere

---
+++ context/dome.lua
@@ -0,0 +1,15 @@
+function mtscad.Context:dome(radius, hollow)
+    local min_radius, max_radius = radius * (radius - 1), radius * (radius + 1)
+	for z = -radius, radius do
+		for y = 0, radius do
+			for x = -radius, radius do
+				local squared = x * x + y * y + z * z
+				if squared <= max_radius and (not hollow or squared >= min_radius) then
+                    self
+                    :translate(x, y, z)
+                    :set_node()
+				end
+			end
+		end
+	end
+end
