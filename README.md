58faea8ec99819e19b957237f0070c4a314ef86c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-27 10:25:55 +0200

    working version

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # mtscad
 
+OpenSCAD inspired minetest mod
+
+Status: **WIP**
+
+# Example
+
+Files placed in `<worlddir>/mtscad/`
+
+rot_test.lua
+```lua
+return function(ctx)
+    ctx
+    :with({ name="wool:red" })
+    :cube(10,1,1)
+    :with({ name="wool:blue" })
+    :rotate(0,90,0)
+    :cube(10,1,1)
+    :with({ name="wool:green" })
+    :rotate(0,90,0)
+    :cube(10,1,1)
+    :with({ name="wool:yellow" })
+    :rotate(0,90,0)
+    :cube(10,1,1)
+    :translate(10,0,0)
+    :with({ name="moreblocks:slope_stone" })
+    :slope({ x=1, y=1, z=0 })
+    :set_node()
+end
+```
+
+test.lua
+```lua
+local rot_test = load("rot_test")
+
+return function(ctx)
+    ctx
+    :with({ name="default:stone" })
+    :cube(10, 1, 1)
+    :cube(1, 10, 1)
+    :cube(1, 1, 10)
+
+    ctx
+    :translate(10, 10, 10)
+    :execute(rot_test)
+
+    ctx
+    :with({ name="default:copperblock" })
+    :translate(2,2,2)
+    :grid(5, 5, 5)
+end
+```
+
+```
+/scad test
+```
--- load.lua
+++ load.lua
@@ -2,19 +2,42 @@
 local path = minetest.get_worldpath() .. "/mtscad"
 minetest.mkdir(path)
 
-minetest.register_chatcommand("scad", {
-    func = function(name, filename)
-        local def, load_err = loadfile(path .. "/" .. filename)
-        if not def then
-            return false, "Loading failed with '" .. load_err .. "'"
-        end
+local function safe_load(modulename)
+    local env = {
+        print = print,
+        dump = dump,
+        load = safe_load
+    }
 
+    local def, load_err = setfenv(loadfile(path .. "/" .. modulename .. ".lua"), env)
+    if not def then
+        return nil, "Loading failed with '" .. load_err .. "'"
+    end
+
+    local fn
+    local success, exec_err = pcall(function()
+        fn = def()
+    end)
+
+    if not success then
+        return nil, "Exec failed with: '" .. exec_err .. "'"
+    end
+
+    return fn
+end
+
+minetest.register_chatcommand("scad", {
+    func = function(name, modulename)
         local player = minetest.get_player_by_name(name)
         local ppos = player:get_pos()
         local ctx = mtscad.create_context(vector.round(ppos))
 
+        local fn, err_msg = safe_load(modulename)
+        if not fn then
+            return false, err_msg
+        end
+
         local success, exec_err = pcall(function()
-            local fn = def()
             fn(ctx)
         end)
 
