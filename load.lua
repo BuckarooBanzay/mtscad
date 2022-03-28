8eb6b6e7d2643331da67f616e990ca445b6716da
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-28 19:27:24 +0200

    main/defaults

--- load.lua
+++ load.lua
@@ -22,25 +22,42 @@ local function load_module(modulename)
     return def()
 end
 
+local function merge_defaults(t, defaults)
+    for k, v in pairs(defaults) do
+        if not t[k] then
+            t[k] = v
+        end
+    end
+end
+
 minetest.register_chatcommand("scad", {
     func = function(name, modulename)
         local player = minetest.get_player_by_name(name)
         local ppos = player:get_pos()
         local ctx = mtscad.create_context(vector.round(ppos))
 
-        local fn
+        local mod
         local success, exec_err = pcall(function()
-            fn = load_module(modulename)
+            mod = load_module(modulename)
         end)
         if not success then
             return false, "Load failed with '" .. exec_err .. "'"
         end
-        if not fn then
+        if not mod then
             return false, "No script loaded"
         end
 
         success, exec_err = pcall(function()
-            fn(ctx)
+            if type(mod) == "function" then
+                mod(ctx)
+            elseif type(mod) == "table" then
+                local opts = {}
+                if type(mod.defaults) == "table" then
+                    merge_defaults(opts, mod.defaults)
+                end
+
+                mod.main(ctx, opts)
+            end
         end)
 
         if not success then
