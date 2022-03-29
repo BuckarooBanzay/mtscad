2a5bca07ac9484009c1ad05c10f1e6195066f95d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-03-29 09:41:32 +0200

    add `rotate_facedir` helper
    source: https://github.com/minetest/minetest/pull/11932

--- util.lua
+++ util.lua
@@ -70,16 +70,58 @@ local wallmounted = {
     [180] = {0, 1, 3, 2, 5, 4, 0, 0},
     [270] = {0, 1, 4, 5, 3, 2, 0, 0}
 }
-local facedir = {
-    [90]  = { 1,  2,  3,  0, 13, 14, 15, 12, 17, 18, 19, 16,
-              9, 10, 11,  8,  5,  6,  7,  4, 23, 20, 21, 22},
-    [180] = { 2,  3,  0,  1, 10, 11,  8,  9,  6,  7,  4,  5,
-             18, 19, 16, 17, 14, 15, 12, 13, 22, 23, 20, 21},
-    [270] = { 3,  0,  1,  2, 19, 16, 17, 18, 15, 12, 13, 14,
-              7,  4,  5,  6, 11,  8,  9, 10, 21, 22, 23, 20}
+
+-- https://github.com/minetest/minetest/pull/11932 / https://github.com/aerkiaga
+-- Map each facedir to the result of applying
+-- right-hand rotation around y+ or z+ vector
+local rotated_facedir_map = {
+	["y+"] = {
+		[0] = 3, 0, 1, 2,
+		19, 16, 17, 18,
+		15, 12, 13, 14,
+		7, 4, 5, 6,
+		11, 8, 9, 10,
+		21, 22, 23, 20
+	},
+	["z+"] = {
+		[0] = 12, 13, 14, 15,
+		7, 4, 5, 6,
+		9, 10, 11, 8,
+		20, 21, 22, 23,
+		0, 1, 2, 3,
+		16, 17, 18, 19,
+	},
 }
 
-local function transform_node_axis(node, _, angle)
+-- Map rotations on all 6 orthogonal vectors to
+-- rotations around just y+ and z+
+local vector_lambda_map = {
+	["x+"] = function (f, r, x)
+		return f(3, "y+", f(r, "z+", f(1, "y+", x))) end,
+	["x-"] = function (f, r, x)
+		return f(1, "y+", f(r, "z+", f(3, "y+", x))) end,
+	["y+"] = function (f, r, x)
+		return f(r, "y+", x) end,
+	["y-"] = function (f, r, x)
+		return f(4 - r, "y+", x) end,
+	["z+"] = function (f, r, x)
+		return f(r, "z+", x) end,
+	["z-"] = function (f, r, x)
+		return f(4 - r, "z+", x) end,
+}
+
+local function rotate_facedir(rotation, vector_name, facedir)
+	facedir = facedir or 0
+	local function lookup_function(r, n, x)
+		if r == 0 then return x end
+		return lookup_function(r - 1, n, rotated_facedir_map[n][x])
+	end
+	local translation_function = vector_lambda_map[vector_name]
+		or function () return nil end
+	return translation_function(lookup_function, rotation % 4, facedir)
+end
+
+local function transform_node_axis(node, axis, angle)
     angle = angle % 360
     if angle == 0 then
         return 0
@@ -89,20 +131,17 @@ local function transform_node_axis(node, _, angle)
     end
 
     local wallmounted_substitution = wallmounted[angle]
-    local facedir_substitution = facedir[angle]
     local def = minetest.registered_nodes[node.name]
     if def then
         local paramtype2 = def.paramtype2
-        if paramtype2 == "wallmounted" or
-                paramtype2 == "colorwallmounted" then
+        if (paramtype2 == "wallmounted" or paramtype2 == "colorwallmounted") and axis == "y+" then
             local orient = node.param2 % 8
             node.param2 = node.param2 - orient +
                     wallmounted_substitution[orient + 1]
-        elseif paramtype2 == "facedir" or
-                paramtype2 == "colorfacedir" then
-            local orient = node.param2 % 32
-            node.param2 = node.param2 - orient +
-                    facedir_substitution[orient + 1]
+
+        elseif paramtype2 == "facedir" or paramtype2 == "colorfacedir" then
+            node.param2 = rotate_facedir(math.floor(angle / 90), axis, node.param2)
+
         end
     end
 
@@ -110,6 +149,8 @@ end
 
 function mtscad.transform_node(node, rotation)
     local tnode = { name=node.name, param1=node.param1, param2=node.param2 }
-    transform_node_axis(tnode, "y", rotation.y)
+    transform_node_axis(tnode, "x+", rotation.x)
+    transform_node_axis(tnode, "y+", rotation.y)
+    transform_node_axis(tnode, "z+", rotation.z)
     return tnode
 end
