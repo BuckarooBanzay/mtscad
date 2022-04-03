763f335ccc36f76592e07c602b4deda63425a389
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-04-03 16:02:28 +0200

    use merge()

--- chat/load.lua
+++ chat/load.lua
@@ -32,14 +32,6 @@ local function load_module(modulename)
     return def()
 end
 
-local function merge_defaults(t, defaults)
-    for k, v in pairs(defaults) do
-        if not t[k] then
-            t[k] = v
-        end
-    end
-end
-
 minetest.register_chatcommand("scad", {
     func = function(name, modulename)
         local origin = mtscad.get_origin(name)
@@ -64,7 +56,7 @@ minetest.register_chatcommand("scad", {
             local opts = {}
             if type(options) == "table" then
                 if type(options.defaults) == "table" then
-                    merge_defaults(opts, options.defaults)
+                    opts = mtscad.merge(options.defaults, opts)
                 end
             end
 
