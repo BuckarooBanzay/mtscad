b7cfc1dce46692f13a17d971d6ed75cd55bab908
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-04-05 17:50:01 +0200

    more translation tests

--- spec/transform_spec.lua
+++ spec/transform_spec.lua
@@ -16,12 +16,12 @@ describe("mtscad.transform_pos() test", function()
 	end)
 	it("works with y=90 rotation", function()
 		local origin = { x=4, y=0, z=0 }
-		local pos = { x=4, y=0, z=1 }
+		local pos = { x=5, y=0, z=0 }
         local rotation = { x=0, y=90, z=0 }
 
         local tpos = mtscad.transform_pos(origin, pos, rotation)
-        assert.equal(5, tpos.x)
+        assert.equal(4, tpos.x)
         assert.equal(0, tpos.y)
-        assert.equal(0, tpos.z)
+        assert.equal(-1, tpos.z)
 	end)
 end)
