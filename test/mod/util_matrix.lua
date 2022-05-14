641421629bb880ed5c40e81bc6f450bbf32057d5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-05-14 12:19:00 +0200

    working combined rotation test

--- test/mod/util_matrix.lua
+++ test/mod/util_matrix.lua
@@ -1,5 +1,7 @@
 
 return function(callback)
+    print("util_matrix")
+
     -- matrix to pos and back
     local pos = { x=10, y=-10, z=20 }
     local m = mtscad.pos_to_matrix(pos)
@@ -10,73 +12,81 @@ return function(callback)
 
     -- rotate around x axis
     pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_x(90), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_x(90))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=0,y=0,z=10}), "90° rotation around x axis")
 
-    pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_x(180), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_x(180))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=0,y=-10,z=0}), "180 rotation around x axis")
 
-    pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_x(270), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_x(270))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=0,y=0,z=-10}), "270 rotation around x axis")
 
     -- rotate around y axis
     pos = {x=10, y=0, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_y(90), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_y(90))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=0,y=0,z=-10}), "90° rotation around y axis")
 
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_y(180), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_y(180))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=-10,y=0,z=0}), "180° rotation around y axis")
 
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_y(270), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_y(270))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=0,y=0,z=10}), "270 rotation around y axis")
 
     -- rotate around z axis
     pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(90), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_z(90))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=-10,y=0,z=0}), "90° rotation around z axis")
 
-    pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(180), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_z(180))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=0,y=-10,z=0}), "180° rotation around z axis")
 
-    pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(270), mtscad.pos_to_matrix(pos))
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_z(270))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=10,y=0,z=0}), "270 rotation around z axis")
 
     -- combined rotations
 
     -- rotate twice 180°
     pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(180), mtscad.pos_to_matrix(pos))
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(180), m)
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_z(180))
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_z(180))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=0,y=10,z=0}), "2x 180° rotation around z axis")
 
     -- rotate 180° + 90° + 90°
     pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(180), mtscad.pos_to_matrix(pos))
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(90), m)
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(90), m)
-    pos2 = mtscad.matrix_to_pos(m)
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_z(180))
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_z(90))
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_z(90))
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
     assert(vector.equals(pos2, {x=0,y=10,z=0}), "1x 180°, 2x 90° rotation around z axis")
 
     -- rotate x-90°, y-90°
     pos = {x=0, y=10, z=0}
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_x(90), mtscad.pos_to_matrix(pos))
-    m = mtscad.multiply_matrix(mtscad.rotation_matrix_y(90), m)
-    pos2 = mtscad.matrix_to_pos(m)
-    assert(vector.equals(pos2, {x=10,y=0,z=0}), "90°X + 90°Y rotation")
+    m = mtscad.rotation_matrix_x(0)
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_x(90), mtscad.pos_to_matrix(pos))
+    m = mtscad.multiply_matrix(m, mtscad.rotation_matrix_y(90), m)
+    pos2 = mtscad.matrix_to_pos(mtscad.multiply_matrix(m, mtscad.pos_to_matrix(pos)))
+    assert(vector.equals(pos2, {x=0,y=0,z=10}), "90°X + 90°Y rotation")
 
     callback()
 end
