324790f0facd81cb786a34e019e7d34ad71495ab
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-05-14 11:39:59 +0200

    combined rotations test

--- test/mod/util_matrix.lua
+++ test/mod/util_matrix.lua
@@ -7,6 +7,7 @@ return function(callback)
     assert(vector.equals(pos, pos2), "pos-matrix-pos conversion")
 
     -- test clock-wise rotations
+
     -- rotate around x axis
     pos = {x=0, y=10, z=0}
     m = mtscad.multiply_matrix(mtscad.rotation_matrix_x(90), mtscad.pos_to_matrix(pos))
@@ -52,6 +53,30 @@ return function(callback)
     m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(270), mtscad.pos_to_matrix(pos))
     pos2 = mtscad.matrix_to_pos(m)
     assert(vector.equals(pos2, {x=10,y=0,z=0}), "270 rotation around z axis")
+
+    -- combined rotations
+
+    -- rotate twice 180°
+    pos = {x=0, y=10, z=0}
+    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(180), mtscad.pos_to_matrix(pos))
+    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(180), m)
+    pos2 = mtscad.matrix_to_pos(m)
+    assert(vector.equals(pos2, {x=0,y=10,z=0}), "2x 180° rotation around z axis")
+
+    -- rotate 180° + 90° + 90°
+    pos = {x=0, y=10, z=0}
+    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(180), mtscad.pos_to_matrix(pos))
+    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(90), m)
+    m = mtscad.multiply_matrix(mtscad.rotation_matrix_z(90), m)
+    pos2 = mtscad.matrix_to_pos(m)
+    assert(vector.equals(pos2, {x=0,y=10,z=0}), "1x 180°, 2x 90° rotation around z axis")
+
+    -- rotate x-90°, y-90°
+    pos = {x=0, y=10, z=0}
+    m = mtscad.multiply_matrix(mtscad.rotation_matrix_x(90), mtscad.pos_to_matrix(pos))
+    m = mtscad.multiply_matrix(mtscad.rotation_matrix_y(90), m)
+    pos2 = mtscad.matrix_to_pos(m)
+    assert(vector.equals(pos2, {x=10,y=0,z=0}), "90°X + 90°Y rotation")
 
     callback()
 end
