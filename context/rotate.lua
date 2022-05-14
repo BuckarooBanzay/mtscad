be7c4c0c2a968fdad79c77f464fa8ca21a70c3f8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-05-14 14:15:52 +0200

    assert rotation-angles

--- context/rotate.lua
+++ context/rotate.lua
@@ -1,5 +1,8 @@
 
 function mtscad.Context:rotate(x, y, z)
+    assert(x % 90 == 0, "x angle is a multiple of 90°")
+    assert(y % 90 == 0, "y angle is a multiple of 90°")
+    assert(z % 90 == 0, "z angle is a multiple of 90°")
     local ctx = self:clone()
     if x ~= 0 then
         ctx.rotation = mtscad.multiply_matrix(ctx.rotation, mtscad.rotation_matrix_x(x))
