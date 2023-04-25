77826592760c2f488782bf6d7e7e2815dad0f633
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-04-25 14:08:54 +0200

    hollow -> fill

--- context/cylinder.lua
+++ context/cylinder.lua
@@ -1,11 +1,11 @@
-function mtscad.Context:cylinder(radius, height, hollow)
+function mtscad.Context:cylinder(radius, height, fill)
 	radius = math.floor(radius + 0.5)
 	local min_radius, max_radius = radius * (radius - 1), radius * (radius + 1)
 	for z = -radius, radius do
 		for y = 0, height-1 do
 			for x = -radius, radius do
 				local squared = x * x + z * z
-				if squared <= max_radius and (not hollow or squared >= min_radius) then
+				if squared <= max_radius and (fill or squared >= min_radius) then
 					self
 					:translate(x, y, z)
 					:set_node()
