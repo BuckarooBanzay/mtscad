9c9ff211f971ed98b8698c97535cf6b2367cf090
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-04-28 11:20:04 +0200

    partial polygon fill fix

--- util/polygon.spec.lua
+++ util/polygon.spec.lua
@@ -20,6 +20,33 @@ mtt.register("get_polygon_crossings", function(callback)
     callback()
 end)
 
+mtt.register("get_polygon_crossings_2", function(callback)
+    local points = {
+        -- x, y (octagon)
+        {0, 5},
+        {0, 10},
+        {5, 15},
+        {10, 15},
+        {15, 10},
+        {15, 5},
+        {10, 0},
+        {5, 0},
+        {0, 5}
+    }
+
+    local crossings = mtscad.get_polygon_crossings(points, 7)
+    assert(#crossings == 2)
+    assert(crossings[1] == 0)
+    assert(crossings[2] == 15)
+
+    crossings = mtscad.get_polygon_crossings(points, 1)
+    assert(#crossings == 2)
+    --assert(crossings[1] == 4) TODO
+    assert(crossings[2] == 11)
+
+    callback()
+end)
+
 mtt.register("is_point_in_polygon", function(callback)
     local points = {
         -- x, y (triangle)
