1632c43d476873d53e3157c81428df698d2b6b5c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-04-28 10:43:34 +0200

    origin fix

--- chat/origin.lua
+++ chat/origin.lua
@@ -1,6 +1,11 @@
 
 minetest.register_chatcommand("scad_origin", {
     func = function(name)
-        mtscad.set_origin(name)
+        local player = minetest.get_player_by_name(name)
+        if player then
+            local pos = vector.round(player:get_pos())
+            mtscad.set_origin(name, pos)
+            return true, "Origin set to " .. minetest.pos_to_string(pos)
+        end
     end
 })
